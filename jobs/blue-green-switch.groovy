8a6870486ed563cc71027548749e434b9b588596
Gordon Clark <Gordon.Clark2@scotland.gsi.gov.uk>
2015-11-03 09:18:21 +0000

    MGS-196 TASK Added blue green switch jenkins job

---
+++ jobs/blue-green-switch.groovy
@@ -0,0 +1,18 @@
+job("blue-green-switch") {
+    parameters {
+        choiceParam('env', ['blu', 'grn'], 'mygov.scot production environment')
+    }
+    scm {
+        git('ssh://git@stash.digital.gov.uk:7999/mgv/aws.git')
+    }
+    steps {
+        shell('''\
+            cd tools/management/
+            echo ./aws_blugrn_switch.sh ${env}
+            echo "INFO: Just switched to [PRDENV] ${env}" \
+            ''')
+    }
+    publishers {
+        buildDescription('\\[PRDENV\\] (blu|grn)')
+    }
+}
