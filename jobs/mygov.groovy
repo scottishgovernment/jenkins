fafdd69e2945d598413c0d0053e35a29a0c851b8
Martin Ellis <martin.ellis@gov.scot>
2015-11-03 14:08:20 +0000

    MGS-196 Add dev deploy scripts.

--- jobs/mygov.groovy
+++ jobs/mygov.groovy
@@ -1,12 +1,30 @@
 import org.yaml.snakeyaml.Yaml
 import scot.mygov.jenkins.*
 
+import static scot.mygov.jenkins.Utils.repo
+
+/**
+ * Returns the workspace of this seed project.
+ */
+def File workspace() {
+    File file = new File(JavaProject.class.getResource('.').toURI())
+    for (int i = 0; i < 6; i++) {
+        file = file.getParentFile()
+    }
+    return file;
+}
+
 def yaml = new Yaml().load(readFileFromWorkspace("resources/mygov.yaml"))
-yaml.get("jobs").each {
+def jobs = yaml.get("jobs")
+jobs.collect {
     new JavaProject(it).build(this, out)
 }
 
-job("versions") {
+new File(workspace(), "jobs.txt").withWriter { out ->
+    jobs.each { out.println(it['name'] + "," + repo(it['repo'])) }
+}
+
+job("Set Build Number") {
   steps {
     shell("cd ../../mygov-seed/workspace")
     shell(readFileFromWorkspace('resources/set-build-id'))
