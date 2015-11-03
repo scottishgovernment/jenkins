13f333691b934b55c95c9ffd10474c38f1b5173a
Martin Ellis <martin.ellis@gov.scot>
2015-11-03 10:48:22 +0000

    MGS-196 Add promotions proceses for each environment.

--- jobs/mygov.groovy
+++ jobs/mygov.groovy
@@ -6,7 +6,7 @@ jobs = [
         repo: 'basic-authentication',
         snapshot: 'authentication-client,authentication-spring',
         host: 'pubapp01',
-        envs: 'both'
+        site: 'both'
     ],
     [
         name: 'Beta Config',
@@ -17,68 +17,68 @@ jobs = [
         name: 'Business Rates',
         repo: 'business-rates-service',
         host: 'pubapp01',
-        envs: 'mygov'
+        site: 'mygov'
     ],
     [
         name: 'Decommission Tool',
         repo: 'decommission-tool',
         host: 'pubapp01',
-        envs: 'both'
+        site: 'both'
     ],
     [
         name: 'Doctor',
         repo: 'doctor',
         host: 'sitapp01',
-        envs: 'gov'
+        site: 'gov'
     ],
     [
         name: 'Feedback',
         repo: 'feedback',
         host: 'fbkapp01',
-        envs: 'both'
+        site: 'both'
     ],
     [
         name: 'Funding Tool',
         repo: 'funding-tool',
         host: 'pubapp01',
-        envs: 'mygov'
+        site: 'mygov'
     ],
     [
         name: 'GeoSearch',
         repo: 'geo-search',
         host: 'sitapp01',
-        envs: 'mygov'
+        site: 'mygov'
     ],
     [
         name: 'Health Check',
         repo: 'health-check',
         host: 'pubapp01',
-        envs: 'both'
+        site: 'both'
     ],
     [
         name: 'Pictor',
         repo: 'pictor',
         host: 'sitapp01',
-        envs: 'gov'
+        site: 'gov'
     ],
     [
         name: 'Press Releases',
         repo: 'press-releases',
         host: 'pubapp01',
-        envs: 'gov'
+        site: 'gov'
     ],
     [
         name: 'Publishing',
         repo: 'beta-publishing',
         snapshot: 'publishing-api',
         host: 'pubapp01',
-        envs: 'both'
+        site: 'both'
     ],
     [
         name: 'Search',
         repo: 'beta-web-site-search',
         host: 'sitapp01',
-        envs: 'both'
+        site: 'both'
     ],
     [
         name: 'Utils',
