712691650da216e4f06f54ae5993b2499f1162f4
Martin Ellis <martin.ellis@gov.scot>
2015-11-06 11:56:24 +0000

    MGS-196 Remove spaces from job directory names.

--- jobs/SiteFailTrigger.groovy
+++ jobs/SiteFailTrigger.groovy
@@ -1,7 +1,8 @@
 import static scot.mygov.jenkins.Utils.repo
 import static scot.mygov.jenkins.Utils.trim
 
-job("MyGov Site Fail Trigger") {
+job('site-fail-trigger') {
+    displayName('MyGov Site Fail Trigger')
     scm {
         git(repo('aws'))
     }
