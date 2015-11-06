c646281ee7e4b5d17e499daa6710a9bdc2ec2d14
Martin Ellis <martin.ellis@gov.scot>
2015-11-06 11:51:01 +0000

    MGS-196 Remove spaces from backup job directory names.

--- jobs/BackupAtlassian.groovy
+++ jobs/BackupAtlassian.groovy
@@ -1,6 +1,21 @@
 import static scot.mygov.jenkins.Utils.trim
 
-job("Backup Confluence") {
+job('backup-jira') {
+    displayName('Backup JIRA')
+    steps {
+        shell(trim('''\
+            set -e
+            scp devops@jira:/tmp/*jira-backup-inc-db.tgz .
+            /usr/local/bin/aws s3 cp ./ s3://scotgovdigitalbackups/jira/ \\
+              --exclude "*" \\
+              --include "*.tgz" \\
+              --recursive \
+        '''))
+    }
+}
+
+job('backup-confluence') {
+    displayName('Backup Confluence')
     steps {
         shell(trim('''\
             set -e
