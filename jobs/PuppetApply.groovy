0f061f8a14f5e57813b0a9cf1d5cae12e7496761
Martin Ellis <martin.ellis@gov.scot>
2015-11-06 19:17:17 +0000

    MGS-196 Allow puppet apply on dev.

--- jobs/PuppetApply.groovy
+++ jobs/PuppetApply.groovy
@@ -3,7 +3,10 @@ import static scot.mygov.jenkins.Utils.repo
 job('puppet-apply') {
     displayName('Puppet Apply')
     parameters {
-        choiceParam('env', ['int', 'exp', 'uat', 'per', 'grn', 'blu'], 'mygov.scot environment')
+        choiceParam(
+            'env',
+            ['dev', 'int', 'exp', 'uat', 'per', 'grn', 'blu'],
+            'mygov.scot environment')
         choiceParam('dbrestore', ['false', 'true'], 'restore databases')
     }
     scm {
