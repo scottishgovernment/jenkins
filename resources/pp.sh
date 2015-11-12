114bddca1d53cc363396446a740ae60d175f1fc5
Martin Ellis <martin.ellis@gov.scot>
2015-11-12 10:41:03 +0000

    MGS-196 Performance platform builds in an unusual way.

---
+++ resources/pp.sh
@@ -0,0 +1,21 @@
+set -ex
+repo=performance_platform
+version="1.0.${BUILD_ID}"
+git clean -fdx
+git update-ref --no-deref HEAD HEAD
+mvn -B \
+  versions:set \
+  versions:use-latest-versions \
+  versions:update-properties \
+  -DnewVersion="${version}" \
+  -Dincludes='org.mygovscot.*,scot.mygov.*' \
+  -DgenerateBackupPoms=false \
+  -Pdebian
+mvn -B versions:update-properties \
+  -Dincludes=mygovscot.utils.version \
+  -DgenerateBackupPoms
+git commit -am "Set version to ${version}"
+git tag -a -m "Build ${version}" ${version}
+git push --tags ssh://git@stash.digital.gov.uk:7999/mgv/${repo}.git "${version}"
+mvn -B -Prelease deploy -Pdebian
+mvn -B verify sonar:sonar -Psonar || true
