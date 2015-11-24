f8084a3ce54eaed9edb25030a20ea8b31e769c89
Gordon Clark <Gordon.Clark2@scotland.gsi.gov.uk>
2015-11-24 17:14:02 +0000

    MGS-423 TASK Modified atlassian jenkins jobs to run in a schedule

--- jobs/ScheduledJobs.groovy
+++ jobs/ScheduledJobs.groovy
@@ -28,6 +28,60 @@ jobs << buildFlowJob('scheduled-teardown-test-envs') {
     '''))
 }
 
+jobs << job('backup-jira') {
+    displayName('Backup JIRA')
+    triggers {
+        cron('00 03 * * 1-5')
+    }
+    steps {
+        shell(trim('''\
+            set -e
+            scp devops@jira:/tmp/*jira-backup-inc-db.tgz .
+            /usr/local/bin/aws s3 cp ./ s3://scotgovdigitalbackups/jira/ \\
+              --exclude "*" \\
+              --include "*.tgz" \\
+              --recursive \
+        '''))
+    }
+}
+
+jobs << job('backup-confluence') {
+    triggers {
+        cron('00 03 * * 1-5')
+    }
+    displayName('Backup Confluence')
+    steps {
+        shell(trim('''\
+            set -e
+            backupfilename=`ssh devops@confluence "ls -lart /home/confluence/confluence-home/backups/backup* |tail -1"`
+            filename=`echo $backupfilename | awk '{ print $9 }'`
+            scp devops@confluence:$filename .
+            /usr/local/bin/aws s3 cp ./ s3://scotgovdigitalbackups/confluence/ \\
+              --exclude "*" \\
+              --include "*.zip" \\
+              --recursive
+        '''))
+    }
+}
+
+jobs << job('backup-stash') {
+    triggers {
+        cron('00 04 * * 1-5')
+    }
+    displayName('Backup Stash')
+    steps {
+        shell(trim('''\
+            set -e
+            ssh devops@stash "cd /home/stash/stash-backup-client-1.3.1 && java -noverify -jar stash-backup-client.jar"
+            scp devops@stash://home/stash/stash-backup-home/backups/*.tar .
+            /usr/local/bin/aws s3 cp ./ s3://scotgovdigitalbackups/stash/ \\
+              --exclude "*" \\
+              --include "*.tar" \\
+              --recursive
+        '''))
+    }
+}
+
 listView('Scheduled Jobs') {
     statusFilter(StatusFilter.ENABLED)
     delegate.jobs {
