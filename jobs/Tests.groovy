f1acd5e1f157dc9afe5e88e52a77ab79bd43863e
Rajesh Yarlagadda <rajesh.yarlagadda@scotland.gsi.gov.uk>
2016-01-29 12:27:38 +0000

    Updated E2ETests jobs to include test environment

--- jobs/Tests.groovy
+++ jobs/Tests.groovy
@@ -155,6 +155,7 @@ jobs << job('accessibility-tests') {
 jobs << job('end-to-end-tests') {
     displayName('End-to-end tests')
     parameters {
+        choiceParam('testenv', ['int', 'exp','per','blu','grn','igv','egv','ugv','pgv','local'], 'Use this option to select test environment against which tests shall be executed')
         choiceParam('mode', ['single', 'multi'], 'Use this option to run the tests only in Chrome (single) or on Chrome, Firefox and Safari (multi)')
         stringParam('selenium_ip_address', '10.21.134.45', 'Use this option to specify the IP address of the machine running Selenium web driver')
         stringParam('tests', 'all', 'Use this option to specify what tests to run. Enter a comma-separated (NO SPACES) list with any combination of these values: webE2E,pubE2E,webSmokeTests,pubSmokeTests,stagingSite')
@@ -164,7 +165,7 @@ jobs << job('end-to-end-tests') {
     }
     steps {
         shell(trim('''\
-            ./run.sh -i ${selenium_ip_address} -m ${mode} -t ${tests}
+            ./run.sh -i ${selenium_ip_address} -m ${mode} -t ${tests} -e ${testenv}
         '''))
     }
     publishers {
