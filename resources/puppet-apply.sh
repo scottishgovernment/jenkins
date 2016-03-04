dc8e041651b0ceb04bb23522c6ef4026e23835b4
Martin Ellis <martin.ellis@gov.scot>
2016-03-04 15:48:02 +0000

    Remove duplicate code in puppet-apply.sh

--- resources/puppet-apply.sh
+++ resources/puppet-apply.sh
@@ -1,16 +1,9 @@
 # Script to run puppet apply on the desired environment
 set -e
-case ${env} in
-  dev)
-    cd tools/management/dev
-    fab -P --set dbrestore=${dbrestore},redisrestore=${redisrestore} ${env} puppet
-    ;;
-  dgv)
-    cd tools/management/dev
-    fab -P, --set dbrestore=${dbrestore},redisrestore=${redisrestore} ${env} puppet
-    ;;
+cd tools/management/dev
+case "${env}" in
+  dev|dgv)
+    fab -P --set dbrestore=${dbrestore},redisrestore=${redisrestore} ${env} puppet;;
   *)
-    cd tools/management/aws_fabric
-    fab -P -z 8 --set dbrestore=${dbrestore},redisrestore=${redisrestore} ${env} apply
-    ;;
+    fab -P -z 8 --set dbrestore=${dbrestore},redisrestore=${redisrestore} ${env} apply;;
 esac
