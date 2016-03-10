b274e03096ec9a538641417e4cacd860cf6d75e2
Doug Kelly <doug.kelly@gov.scot>
2016-03-10 09:58:45 +0000

    Amended Jenkins gov.scot jobs to use the rds build script instead of the old non-rds one

--- jobs/Environments.groovy
+++ jobs/Environments.groovy
@@ -21,12 +21,12 @@ scripts = [
     ],
     'gov': [
         'full': [
-            'up': 'tools/provisioning/vpc/aws_build_full_env_govscot.sh ${env}',
-            'down': 'tools/provisioning/vpc/aws_teardown_full_env_govscot.sh ${env}_vpc'
+            'up': 'tools/provisioning/vpc/aws_build_full_env_govscot_rds.sh ${env}',
+            'down': 'tools/provisioning/vpc/aws_teardown_full_env_govscot_rds.sh ${env}_vpc'
         ],
         'test': [
-            'up': 'tools/provisioning/vpc/aws_build_env_govscot.sh ${env}',
-            'down': 'tools/provisioning/vpc/aws_teardown_env_govscot.sh ${env}_vpc'
+            'up': 'tools/provisioning/vpc/aws_build_env_govscot_rds.sh ${env}',
+            'down': 'tools/provisioning/vpc/aws_teardown_env_govscot_rds.sh ${env}_vpc'
         ]
     ]
 ]
