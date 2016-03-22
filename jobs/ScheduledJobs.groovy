91be62494266034d4ac99544a3daeac779172a4e
Doug Kelly <doug.kelly@gov.scot>
2016-03-22 11:01:41 +0000

    Changes to test environments and prod now bgv and ggv are in place

--- jobs/ScheduledJobs.groovy
+++ jobs/ScheduledJobs.groovy
@@ -14,7 +14,7 @@ jobs << buildFlowJob('scheduled-rebuild-test-envs') {
         }
     }
     buildFlow(trim('''
-        build("gov-test-up", env: "igv")\n
+        build("gov-test-up", env: "ugv")\n
         build("mygov-test-up", env: "int")\n
         build("mygov-test-up", env: "exp")\n
         build("mygov-full-up", env: "per")
@@ -30,7 +30,7 @@ jobs << buildFlowJob('scheduled-teardown-test-envs') {
       }
     }
     buildFlow(trim('''
-        build("gov-test-down", env: "igv")\n
+        build("gov-test-down", env: "ugv")\n
         build("mygov-test-down", env: "int")\n
         build("mygov-test-down", env: "exp")\n
         build("mygov-full-down", env: "per")
