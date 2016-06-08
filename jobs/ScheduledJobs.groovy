b1ede30944e231009d6e248bf554eb3cf0307892
Martin Ellis <martin.ellis@gov.scot>
2016-06-08 09:20:03 +0100

    MGS-1080 Update path for SonarQube backup on repo.

--- jobs/ScheduledJobs.groovy
+++ jobs/ScheduledJobs.groovy
@@ -148,7 +148,7 @@ jobs << job('backup-sonar') {
         ssh devops@sonar "sudo su - sonar -c 'sonarqube-db backup'"
         scp devops@sonar:/opt/sonar/backups/sonarqube.ar .
         aws s3 cp sonarqube.ar s3://scotgovdigitalbackups/sonar/sonarqube.ar
-        scp sonarqube.ar devops@repo:DBbackups/services/sonarqube.ar
+        scp sonarqube.ar devops@repo:/srv/backups/services/sonarqube.ar
         rm sonarqube.ar
         '''))
     }
