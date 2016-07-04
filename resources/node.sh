bb2bb62fa6d01458d64f1a1b3ab575efc46b6bb9
Martin Ellis <martin.ellis@gov.scot>
2016-07-04 15:46:49 +0100

    Convert site-builder to use standard node build.

--- resources/node.sh
+++ resources/node.sh
@@ -5,7 +5,27 @@ version="1.0.${BUILD_ID}"
 git clean -fdx -e node_modules
 git update-ref --no-deref HEAD HEAD
 
-%dependencies%
+dependencies="%dependencies%"
+if [ -n "$dependencies" ]; then
+  # Install dependencies not managed through npm-shrinkwrap.json
+  tmp=$(mktemp -d /tmp/npm-XXXXXX)
+  trap "rm -r \"$tmp\"" 0
+  cd "$tmp"
+  ln -s $OLDPWD lib
+  globalconfig=$(npm config get globalconfig)
+  globalignorefile=$(npm config get globalignorefile)
+  args="--globalconfig=$globalconfig \
+    --globalignorefile=$globalignorefile \
+    --prefix=$PWD"
+  for package in $dependencies; do
+    npm install $args -g --production "$package@latest"
+  done
+  cd -
+fi
+
+jq .version=\"${version}\" package.json | sponge package.json
+git add package.json
+git commit -m "Set version to ${version}"
 ./build --ci -v "${version}"
 
 git tag -a -m "Build ${version}" ${version}
