e06027fc1ce98e5e0e8885753981ee5d820fe3d4
Martin Ellis <martin.ellis@gov.scot>
2016-07-26 10:36:16 +0100

    MGS-1274 Add build job for AWS VPC scripts.
    * Add build job for AWS project.
    * Move puppet job build script into ShellProject.
    * Reuse puppet job for AWS project build.

--- jobs/build/ShellProject.groovy
+++ jobs/build/ShellProject.groovy
@@ -2,12 +2,54 @@ package build
 
 import javaposse.jobdsl.dsl.helpers.step.StepContext
 
+import static build.Utils.trim
+
 class ShellProject extends MyGovProject {
 
     String build
 
     def void build(def StepContext delegate) {
-        delegate.shell(dsl.readFileFromWorkspace('resources/' + build))
+        def script = StringBuilder.newInstance()
+        script << trim("""\
+            #!/bin/sh
+            set -ex
+            repo=${repo}
+            """)
+        if (maven) {
+            def splits = maven.split(':')
+            def groupId = splits[0]
+            def artifactId = splits[1]
+            script << trim("""\
+                debian=${debian}
+                groupId=${groupId}
+                artifactId=${artifactId}
+                version="1.0.\${BUILD_ID}"\n
+                """)
+        }
+        script << trim('''\
+            git tag -a -m "Build ${version}" ${version}
+            git push --tags ssh://git@stash.digital.gov.uk:7999/mgv/${repo}.git "${version}"
+            ''')
+
+        if (build) {
+            script << build << '\n\n'
+        }
+
+        if (maven) {
+          script << trim('''\
+              mvn deploy:deploy-file \\
+                --batch-mode \\
+                -Dfile="${debian}_${version}_all.deb" \\
+                -DgroupId="${groupId}" \\
+                -DartifactId="${artifactId}" \\
+                -Dversion="${version}" \\
+                -Dpackaging=deb \\
+                -DrepositoryId=release \\
+                -Durl=http://repo.digital.gov.uk/content/repositories/releases/
+              ''')
+        }
+
+        delegate.shell(script.toString())
     }
 
 }
