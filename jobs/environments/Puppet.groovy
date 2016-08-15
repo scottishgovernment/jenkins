443b6b2b58f732dfe4ad22b4dfcf9e7c0cee03b7
Martin Ellis <martin.ellis@gov.scot>
2016-08-15 11:55:32 +0100

    Revert "MGS-1103 Dev and dgv environments moved to AWS"
    Dev and dgv not ready to move to AWS yet.

--- jobs/environments/Puppet.groovy
+++ jobs/environments/Puppet.groovy
@@ -18,13 +18,12 @@ def build(site, List<String> envs) {
         }
         steps {
             shell((trim("""\
-              #!/bin/sh -e
               if [ "\$dbrestore" = "true" ]; then
                 tools/management/s3_restore ${site.domain} \${env}
               fi
-              cd tools/management/aws_fabric
-              fab -P -z 8 --set dbrestore=\${dbrestore},redisrestore=\${redisrestore} \${env} apply
             """)))
+            shell(dsl.readFileFromWorkspace('resources/puppet-apply.sh'))
         }
     }
 }
+
---
+++ resources/puppet-apply.sh
@@ -0,0 +1,10 @@
+# Script to run puppet apply on the desired environment
+set -e
+case "${env}" in
+  dev|dgv)
+    cd tools/management/dev
+    fab -P --set dbrestore=${dbrestore},redisrestore=${redisrestore} ${env} puppet;;
+  *)
+    cd tools/management/aws_fabric
+    fab -P -z 8 --set dbrestore=${dbrestore},redisrestore=${redisrestore} ${env} apply;;
+esac
