7bb1f7bc965c07dad0f6115c563a2c42d32565f8
Martin Ellis <martin.ellis@gov.scot>
2016-08-17 12:02:41 +0100

    MGS-1578 Ensure imagesrestore parameter is passed to puppet apply.

--- resources/puppet-apply.sh
+++ resources/puppet-apply.sh
@@ -3,8 +3,8 @@ set -e
 case "${env}" in
   dev|dgv)
     cd tools/management/dev
-    fab -P --set dbrestore=${dbrestore},redisrestore=${redisrestore} ${env} puppet;;
+    fab -P --set dbrestore=${dbrestore},imagesrestore=${imagesrestore},redisrestore=${redisrestore} ${env} puppet;;
   *)
     cd tools/management/aws_fabric
-    fab -P -z 8 --set dbrestore=${dbrestore},redisrestore=${redisrestore} ${env} apply;;
+    fab -P -z 8 --set dbrestore=${dbrestore},imagesrestore=${imagesrestore},redisrestore=${redisrestore} ${env} apply;;
 esac
