568078853e6870ac4e655e32c79527e2f267dd3e
Martin Ellis <martin.ellis@gov.scot>
2016-11-04 14:06:13 +0000

    MGS-1742 Fix path for pipeline deb to be installed.

--- resources/pipeline-deploy.sh
+++ resources/pipeline-deploy.sh
@@ -1,6 +1,6 @@
 set -ex
 name="pipeline"
-path="scot/mygov/pipeline/pipeline/1.0.${BUILD_ID}/pipeline-1.0.${BUILD_ID}.deb"
+path="scot/mygov/pipeline/pipeline/1.0.${PROMOTED_ID}/pipeline-1.0.${PROMOTED_ID}.deb"
 repo=http://nexus/content/repositories/releases/
 curl -sSf -o "${name}.deb" "${repo}/${path}"
-sudo dpkg -i pipeline*.deb
+sudo dpkg -i "${name}.deb"
