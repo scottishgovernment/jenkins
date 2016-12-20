fc6fbb234b4382e94259ed8b0972cb0dfde9229f
Gabor Nagy <gabor.nagy@gov.scot>
2016-12-20 17:09:47 +0000

    MGS-1722 Added missing bits

--- jobs/Services.groovy
+++ jobs/Services.groovy
@@ -1,10 +1,12 @@
 import services.Promotion
+import services.Apply
 
 def jobs = []
 Binding binding = new Binding()
 binding.setVariable("dsl", this)
 binding.setVariable("out", out)
 jobs << new Promotion(binding).build()
+jobs << new Apply(binding).build()
 
 listView('Services') {
     statusFilter(StatusFilter.ENABLED)
