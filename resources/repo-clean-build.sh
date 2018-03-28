b06632f16a777d3618e560321d4e617ef607b5bb
Rachel Willmer <Rachel.Willmer@gov.scot>
2018-03-28 14:30:13 +0100

    MGS-3509 build repo-clean in a docker container

--- resources/repo-clean-build.sh
+++ resources/repo-clean-build.sh
@@ -2,7 +2,7 @@ set -ex
 repo=repo-clean
 version=1.0.${BUILD_ID}
 git clean -fdx
-./build -v $version
+./build --ci -v $version
 git tag -a -m "Build ${version}" ${version}
 git push --tags ssh://git@stash.digital.gov.uk:7999/mgv/${repo}.git "${version}"
 mvn deploy:deploy-file \
