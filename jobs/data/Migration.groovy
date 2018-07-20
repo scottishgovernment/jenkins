751082b9c03ac0e606347bb63d45b891139659c0
David Sinclair <david.sinclair@gov.scot>
2018-07-20 15:32:39 +0100

    Enable feeback migration

--- jobs/data/Migration.groovy
+++ jobs/data/Migration.groovy
@@ -32,8 +32,6 @@ def migrate(site, envs) {
                 release : migrate all content required for a release,
                 test    : migrate all content required for a test environment
             '''))
-
-            booleanParam('feedback', false, 'Include feedback migration')
         }
 
         logRotator {
