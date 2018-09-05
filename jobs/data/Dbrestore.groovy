e8184da94443017ca8b40b3bde795cd81792e9ea
Rachel Willmer <Rachel.Willmer@gov.scot>
2018-09-05 13:49:46 +0100

    MGS-4447 include s3_restore in dbrestore jenkins job
    Add the s3_restore code to the jenkins job which already
    restores the RDS and mongodb databases.

--- jobs/data/Dbrestore.groovy
+++ jobs/data/Dbrestore.groovy
@@ -1,14 +1,23 @@
 package data
 
+import static build.Utils.awsRepo
+
 def build(site, List<String> envs) {
     dsl.job("dbrestore-${site.id}") {
 
-        displayName("Restore data for ${site.domain}")
+        displayName("Restore RDS, mongodb and s3 data for ${site.domain}")
 
         parameters {
             choiceParam('env', envs, 'Environment to which production data is copied')
         }
+        scm {
+            awsRepo(delegate)
+        }
         steps {
+          // Duplication of Restore.groovy
+          shell("tools/management/s3_restore ${site.domain} \${env}")
+
+          // RDS and mongodb
           def script = dsl.readFileFromWorkspace('resources/dbrestore')
           shell(script)
         }
