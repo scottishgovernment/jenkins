f8f616ea7a8590e2619602738589d1628fff6654
Martin Ellis <martin.ellis@gov.scot>
2018-10-19 14:31:58 +0100

    Add job for economicactionplan.mygov.scot

---
+++ jobs/EconomicActionPlan.groovy
@@ -0,0 +1,36 @@
+import static build.Utils.repo
+import static build.Utils.trim
+
+def jobs = []
+
+jobs << job('economic-action-plan') {
+    displayName('Economic Action Plan')
+
+    logRotator {
+        daysToKeep(60)
+    }
+
+    scm {
+        git(repo('economicactionplan.mygov.scot'), 'master')
+    }
+
+    steps {
+        shell(trim('''\
+            set -eux
+            ./build
+            aws s3 sync --delete --acl public-read _site/ s3://economicactionplan.mygov.scot
+        '''))
+    }
+
+    publishers {
+        slackNotifier {
+            notifyAborted(true)
+            notifyFailure(true)
+            notifyNotBuilt(true)
+            notifyUnstable(true)
+            notifyBackToNormal(true)
+            notifyRepeatedFailure(true)
+        }
+    }
+
+}
