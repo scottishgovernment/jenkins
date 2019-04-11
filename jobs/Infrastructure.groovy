0d1f6c515f762831ccf5d0bdf586451206a0ce3a
Martin Ellis <martin.ellis@gov.scot>
2019-04-11 09:39:00 +0100

    Fix string escaping in AMI jobs

--- jobs/Infrastructure.groovy
+++ jobs/Infrastructure.groovy
@@ -14,42 +14,39 @@ sites.each { site ->
             script << trim("""\
                 set -ex
                 repo=packer
+                site=${site}
                 version=${site}-\${override:-\$BUILD_ID}
-
+            """)
+            script << trim('''\
                 git clean -fdx
                 git update-ref --no-deref HEAD HEAD
-                git tag -a -m "Build \${version}" \${version}
+                git tag -a -m "Build ${version}" ${version}
+                git push --tags ssh://git@stash.digital.gov.uk:7999/mgv/${repo}.git "${version}"
 
-                git push --tags ssh://git@stash.digital.gov.uk:7999/mgv/\${repo}.git "\${version}"
-            """)
-            script << trim("""\
-                packer validate \\
-                    -var ami_name=${site}-\${override:-\$BUILD_ID} \\
-                    templates/aws.json
-            """)
-            script << trim("""\
                 export PACKER_NO_COLOR=true
+                packer validate \\
+                    -var ami_name=${site}-${override:-$BUILD_ID} \\
+                    templates/aws.json
                 packer build \\
-                    -var ami_name=${site}-\${override:-\$BUILD_ID} \\
+                    -var ami_name=${site}-${override:-$BUILD_ID} \\
                     templates/aws.json -machine-readable | tee build.log
-                    ami_id=\$(awk -F, '$5=="id" {sub("[a-z0-9-]*:", "", $6); print $6;exit;}' build.log)
-            """)
-            script << trim("""\
-            echo '{}' | jq ".artifactId=\\"${site}\\"
-              | .name=\\"${site}-\${BUILD_ID}\\"
-              | .version=\${BUILD_ID}
-              | .ami=\\"\${ami_id}\\"" > ami.json
+                ami_id=$(awk -F, '$5=="id" {sub("[a-z0-9-]*:", "", $6); print $6; exit;}' build.log)
 
-            mvn deploy:deploy-file \\
-            -DgroupId=scot.mygov.ami \\
-            -DartifactId=${site} \\
-            -Dversion=\${BUILD_ID} \\
-            -DrepositoryId=release \\
-            -Dpackaging=json \\
-            -DgeneratePom \\
-            -Durl=http://nexus/repository/releases/ \\
-            -Dfile=ami.json
-            """)
+                echo '{}' | jq ".artifactId=\\"${site}\\"
+                  | .name=\\"${site}-${BUILD_ID}\\"
+                  | .version=${BUILD_ID}
+                  | .ami=\\"${ami_id}\\"" > ami.json
+
+                mvn deploy:deploy-file \\
+                  -DgroupId=scot.mygov.ami \\
+                  -DartifactId=${site} \\
+                  -Dversion=${BUILD_ID} \\
+                  -DrepositoryId=release \\
+                  -Dpackaging=json \\
+                  -DgeneratePom \\
+                  -Durl=http://nexus/repository/releases/ \\
+                  -Dfile=ami.json
+            ''')
             shell(script.toString())
         }
         properties {
