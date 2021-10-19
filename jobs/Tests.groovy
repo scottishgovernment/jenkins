8550cc47dfd10d8d9634427b5ed35101aa0f3cb8
Martin Ellis <martin.ellis@gov.scot>
2021-10-19 12:51:25 +0100

    Remove install_backstopJS argument from jobs that use perceptual tests

--- jobs/Tests.groovy
+++ jobs/Tests.groovy
@@ -527,7 +527,6 @@ jobs << pipelineJob('integration-test-mygov') {
             stage('mygov-perceptual') {
                 catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
                     build job: 'mygov-perceptual-testing', parameters: [
-                        string(name: 'install_backstopJS', value: 'true'),
                         string(name: 'testEnv', value: 'int'),
                         string(name: 'referenceEnv', value: 'live')
                     ]
@@ -596,7 +595,6 @@ jobs << pipelineJob('integration-test-gov') {
             stage('gov-perceptual') {
                 catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
                     build job: 'gov-perceptual-testing', parameters: [
-                        string(name: 'install_backstopJS', value: 'true'),
                         string(name: 'testEnv', value: 'igv'),
                         string(name: 'referenceEnv', value: 'live')
                     ]
