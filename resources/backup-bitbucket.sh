01ad4920eae7fe5a99967cb32f22b131ef9fdd55
Michael Irwin <michael.irwin@gov.scot>
2023-05-03 14:14:57 +0100

    Use bitbucket CNAME when backing up Bitbucket

--- resources/backup-bitbucket.sh
+++ resources/backup-bitbucket.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
-set -e
+set -eu
+remote=devops@bitbucket.digital.gov.uk
 
-ssh -o StrictHostKeyChecking=no devops@stash "sudo su - stash -c 'cd bitbucket-backup-client-3.2.0 && java -noverify -jar bitbucket-backup-client.jar'"
-scp devops@stash://home/stash/stash-backup-home/backups/*.tar .
+# Runs a command as the stash user
+run() {
+  cmd="$1"
+  command ssh \
+  -o StrictHostKeyChecking=accept-new \
+  -o RequestTTY=no \
+  "$remote" \
+  "sudo su - stash -c '$cmd'"
+}
+
+run '/bin/sh -eu' <<EOF
+cd bitbucket-backup-client-3.2.0
+java -noverify -jar bitbucket-backup-client.jar
+EOF
+
+scp "${remote}://home/stash/stash-backup-home/backups/*.tar" .
 
 aws s3api put-object --bucket scotgovdigitalbackups --key bitbucket/bitbucket_latest.tar --body *.tar
 
 rm -rf *.tar
-ssh devops@stash "sudo su - stash -c 'rm -rf /home/stash/stash-backup-home/backups/*.tar'"
+run 'rm -rf /home/stash/stash-backup-home/backups/*.tar'
