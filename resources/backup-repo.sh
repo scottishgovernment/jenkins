a91ba4f851b38eb91fdb716a4414f9b302387953
Martin Ellis <martin.ellis@gov.scot>
2023-06-19 18:53:47 +0100

    Add --no-progress flag to s3 sync for database backups

--- resources/backup-repo.sh
+++ resources/backup-repo.sh
@@ -2,4 +2,4 @@
 ssh -o StrictHostKeyChecking=no devops@repo s3-nexus backup
 ssh devops@repo s3-apt backup
 ssh devops@repo s3-apt-cacher-ng backup
-ssh devops@repo aws s3 sync --delete /srv/backups/ s3://mgs-repo/backups/
+ssh devops@repo aws s3 sync --delete --no-progress /srv/backups/ s3://mgs-repo/backups/
