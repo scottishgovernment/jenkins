bb72709cd2d73e46d70743a9c80478df40c94b50
Martin Ellis <martin.ellis@gov.scot>
2024-02-22 12:31:40 +0000

    Publish snapshots for all hippo-govscot modules

--- jobs/build/JavaProject.groovy
+++ jobs/build/JavaProject.groovy
@@ -22,6 +22,7 @@ class JavaProject extends MyGovProject {
             version: version,
             repo: repo,
             snapshot: snapshot,
+            snapshotAll: snapshot == '*',
             sonarCompatibleJdk: this.jdk != 'openjdk-8'
         ])
 
